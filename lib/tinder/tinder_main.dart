37f276e72dd449e7cdd3fef67a1f511f8278f5c2
AgTears <yauhoman@gmail.com>
2022-05-13 14:35:46 +0800

    tinder page added

---
+++ lib/tinder/tinder_main.dart
@@ -0,0 +1,16 @@
+import 'package:swipe_cards/swipe_cards.dart';
+import 'package:flutter/material.dart';
+
+class TinderPage extends StatefulWidget {
+  TinderPage({Key? key}) : super(key: key);
+
+  @override
+  State<TinderPage> createState() => _TinderPageState();
+}
+
+class _TinderPageState extends State<TinderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
