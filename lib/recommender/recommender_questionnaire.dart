dfc4a029d776711d7e282a21305cc5ef799ef861
Katabatix <huinathanc@gmail.com>
2022-05-14 10:17:38 +0800

    Finished questionnaire skeleton

--- lib/recommender/recommender_questionnaire.dart
+++ lib/recommender/recommender_questionnaire.dart
@@ -16,78 +16,163 @@ class _RecommenderQuestionnaireMainState extends State<RecommenderQuestionnaireM
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> questions = [
-      _Question1(
-        callback: (tag) {
-          setState(() {
-            tags.add(tag);
-            _qNumber++;
-          });
-        },),
-      _Question2(callback: (tag) {
-        setState(() {
-          tags.add(tag);
-          _qNumber++;
-        });
-      },),
-      //Add more questions here
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Recommend Me A Drink"),
       ),
       body: Column(
         children: [
-          questions[_qNumber],
-          TextButton(onPressed: () {
-            print(tags);
-            print(_qNumber);
-          }, child: Text("touch me")),
+          _Question(
+            _qNumber,
+            callback: (tag) {
+              setState(() {
+                tags.add(tag);
+                _qNumber++;
+              });
+            },
+          ),
         ],
       ),
     );
   }
 }
 
-class _Question1 extends StatelessWidget {
-  // const _Question1({Key? key}) : super(key: key);
+class QuestionPrompt {
+  String prompt;
+  List<String> responses;
+  List<String> tags;
 
+  QuestionPrompt(this.prompt, this.responses, this.tags);
+}
+
+class _Question extends StatelessWidget {
+  // const _Question1({Key? key}) : super(key: key);
+  final _qNum;
   final StringCallback callback;
 
-  _Question1({required this.callback});
+  _Question(this._qNum, {required this.callback});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextButton(
-          onPressed: () => callback("tag1"),
-          child: const Text("Add tag1"),
-        ),
-        TextButton(
-          onPressed: () => callback("tag2"),
-          child: const Text("Add tag2"),
-        ),
+  static List<QuestionPrompt> questionPrompts = [
+    QuestionPrompt(
+      "How are you feeling?",
+      [
+        "Looking for a good time!",
+        "Overwhelming giddiness",
+        "Feeling a little quieter",
+        "Stressed out",
       ],
+      [
+        "Happy",
+        "Excited",
+        "Sad",
+        "Stressed",
+      ]
+    ),
+    QuestionPrompt(
+      "How are you feeling?1",
+      [
+        "Looking for a good time!",
+        "Overwhelming giddiness",
+        "Feeling a little quieter",
+        "Stressed out",
+      ],
+      [
+        "Happy",
+        "Excited",
+        "Sad",
+        "Stressed",
+      ]
+    ),
+    QuestionPrompt(
+      "How are you feeling?2",
+      [
+        "Looking for a good time!",
+        "Overwhelming giddiness",
+        "Feeling a little quieter",
+        "Stressed out",
+      ],
+      [
+        "Happy",
+        "Excited",
+        "Sad",
+        "Stressed",
+      ]
+    ),
+    QuestionPrompt(
+      "How are you feeling?3",
+      [
+        "Looking for a good time!",
+        "Overwhelming giddiness",
+        "Feeling a little quieter",
+        "Stressed out",
+      ],
+      [
+        "Happy",
+        "Excited",
+        "Sad",
+        "Stressed",
+      ]
+    ),
+  ];
+
+  Widget makeButton(int buttonNum){
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: (
+            OutlinedButton(
+              onPressed: () => callback(questionPrompts[_qNum].tags[buttonNum]),
+              child: Text(questionPrompts[_qNum].responses[buttonNum],
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(10),
+              ),
+            ),
+          )
+        ),
+      ),
     );
   }
-}
-
-class _Question2 extends StatelessWidget {
-  // const _Question1({Key? key}) : super(key: key);
-
-  final StringCallback callback;
-
-  _Question2({required this.callback});
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => callback("tag3"),
-      child: const Text("Add tag3"),
+
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(height: 150),
+          Column(
+            children: [
+              Text(questionPrompts[_qNum].prompt, style: const TextStyle(
+                fontSize: 30,
+              )),
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  makeButton(0),
+                  makeButton(1),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              Row(
+                children: [
+                  makeButton(2),
+                  makeButton(3),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          SizedBox(height: 150),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      ),
     );
   }
 }
-
 
